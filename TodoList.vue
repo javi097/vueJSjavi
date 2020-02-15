8ab359d9b8f826e7f3055b82a7d205acf16aa658
JaviGarcia <55480199+javi097@users.noreply.github.com>
2020-02-15 18:37:32 +0100

    Creacion fichero vue
    creacion fichero vue

---
+++ TodoList.vue
@@ -0,0 +1,13 @@
+<template>
+    <ul>
+        <todo-item 
+            v-for="(todo,index) in todos"
+            :key="index"
+            :todo="todo">
+        </todo-item>
+    </ul>
+</template>
+
+<style scoped>
+
+</style>
